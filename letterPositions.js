9bf8a46b4ddb2dd082d94c009b57bceb9109a05d
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-01 14:40:40 +0000

    Implemented letterPosition function

---
+++ letterPositions.js
@@ -0,0 +1,33 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  sentence = sentence.replace(/\s/g, '');
+
+  for (let i = 0; i < sentence.length; i++) {
+    let letter = sentence[i];
+    if (!results[letter]) {
+      results[letter] = [];
+    }
+    results[letter].push(i);
+  }
+  return results;
+};
+
+// CHECK IF TWO ARRAYS ARE EQUAL
+const eqArrays = function(arr1, arr2) {
+  if (arr1.every((element, index) => element === arr2[index])) {
+    return true;
+  }
+  return false;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log("✅ Assertion Passed: Arrays are equal! " + arr1 + ' === ' + arr2);
+  } else {
+    console.log("⛔️ Assertion Failed: Arrays are NOT equal! " + arr1 + ' !==' + arr2);
+  }
+};
+
+console.log(letterPositions("hello"));
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
