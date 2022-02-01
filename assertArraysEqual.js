be586cc390883a1cd0cdbc59f44551b16be7d65b
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-01 00:14:26 +0000

    improved eqArrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,5 @@
+// CHECK IF TWO ARRAYS ARE EQUAL
 const eqArrays = function(arr1, arr2) {
-  // ALTERNATIVE SOLUTION USING EVERY()
-  // Checks every element in arr1 and compares it with the rest preceding =>
-  // in this case, if the element from arr1 === arr2, return true
   if (arr1.every((element, index) => element === arr2[index])){
     return true;
   }
@@ -10,11 +8,12 @@ const eqArrays = function(arr1, arr2) {
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)){
-    console.log("✅ Perfect match! " + arr1 + ' === ' + arr2);
+    console.log("✅ Arrays are equal! " + arr1 + ' === ' + arr2);
   } else{
-    console.log("🛑 Not a perfect match! " + arr1 + ' !==' + arr2)
+    console.log("🛑 Arrays are NOT equal! " + arr1 + ' !==' + arr2)
   }
 }
+
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [3, 2, 1]);
 assertArraysEqual(['1', '2', '3'], ['1', '2', '3']);
