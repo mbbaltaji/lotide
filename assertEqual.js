4f03ac0d9dfad06aa2bccb9ed81c527c4c33fce1
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-01 14:34:35 +0000

    Improved testing code

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 //FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`⛔️ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
