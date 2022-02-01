94aaa3577e453c2fa652d1cafd64138f8151feb6
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-01 15:40:24 +0000

    feat: find keys of object by value

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+
+
+const findKeyByValue = function(obj, val) {
+  // AQUIRES ALL KEYS OF obj AND PUTS THEM IN AN ARRAY 
+  const keys = Object.keys(obj);
+  for (const key of keys) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+};
+
+//FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
