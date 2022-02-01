25b1532837e6dae480aecd0bf38167a4d9aabca6
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-01 22:45:58 +0000

    fixed bug

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,21 @@
 // CHECK IF TWO ARRAYS ARE EQUAL
 const eqArrays = function(arr1, arr2) {
-  if (arr1.every((element, index) => element === arr2[index])){
+  if (arr1.length !== arr2.length)
+    return false;
+  
+  else if (arr1.every((element, index) => element === arr2[index])) {
     return true;
   }
   return false;
 };
 
 const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)){
+  if (eqArrays(arr1, arr2)) {
     console.log("✅ Assertion Passed: Arrays are equal! " + arr1 + ' === ' + arr2);
-  } else{
-    console.log("⛔️ Assertion Failed: Arrays are NOT equal! " + arr1 + ' !==' + arr2)
+  } else {
+    console.log("⛔️ Assertion Failed: Arrays are NOT equal! " + arr1 + ' !==' + arr2);
   }
-}
+};
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [3, 2, 1]);
