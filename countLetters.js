12756ee4cf106ba2a54506d33208cc79af0c9e11
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-01 02:20:45 +0000

    added coutLetters function

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  //REMOVE WHITE SPACE
+  sentence = sentence.replace(/\s/g, '');
+  let letterCount = {};
+
+  for (const letter of sentence) {
+    if (letterCount[letter]) {
+      letterCount[letter]++;
+    } else {
+      letterCount[letter] = 1;
+    }
+  }
+  return letterCount;
+};
+
+let actual = countLetters("lighthouse in the house");
+let expected = { l: 1, i: 2, g: 1, h: 4,
+  t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 };
+
+assertEqual(actual.toString(), expected.toString());
