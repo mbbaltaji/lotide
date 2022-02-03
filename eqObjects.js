289a644ddbd4fa37c6bee2d87c8117cc83d6da7e
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-03 17:06:29 +0000

    fixed bugs

--- eqObjects.js
+++ eqObjects.js
@@ -1,23 +1,21 @@
-//FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅ Assertion Passed: Objects are equal! ${actual} === ${expected}`);
   } else {
-    console.log(`⛔️ Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`⛔️ Assertion Failed: Objects are NOT equal ${actual} !== ${expected}`);
   }
 };
 
 // CHECK IF TWO ARRAYS ARE EQUAL
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
+  if (arr1.length !== arr2.length)
     return false;
-  }
-  if (arr1.every((element, index) => element === arr2[index])) {
+  
+  else if (arr1.every((element, index) => element === arr2[index])) {
     return true;
   }
   return false;
-  
-  // LONG WAY USING FOOR LOOP
+  //LONG WAY USING FOOR LOOP
   // if (arr1.length !== arr2.length) {
   //   return false;
   // }
@@ -27,45 +25,38 @@ const eqArrays = function(arr1, arr2) {
   //   }
   // }
   // return false;
-  
 };
 
 const eqObjects = function(obj1, obj2) {
-  // CHECK IF LENGTH MATCHES
+  // CHECK IF OBJECTS HAVE SAME NUMBER OF KEYS
   if (Object.keys(obj1).length !== Object.keys(obj2).length) {
     return false;
   }
-
+  // CHECK IF VALUES OF BOTH OBJECTS ARE EQUAL
   for (const key of Object.keys(obj1)) {
-    // CHECK IF KEY VALUES IS ARRAY
-    if (Array.isArray(Object.keys(obj1[key])) &&
-    Array.isArray(Object.keys(obj2[key]))) {
-      //COMAPRE THE TWO ARRAYS OF EACH KEY
-      if (!eqArrays(Object.keys(obj1[key]), Object.keys(obj2[key]))) {
+    // IF VALUES ARE ARRAYS, CHECK IF ARRAYS ARE EQUAL
+    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+      // RETURN TRUE IF ARRAYS ARE EQUAL
+      return eqArrays(obj1[key], obj2[key]);
+    } else {
+      if (obj1[key] !== obj2[key]) {
         return false;
       }
-    }
-    // IF NOT ARRAY COMAPRE THE PRIMITIVE VALUES
-    else if (obj1[key] !== obj2[key]) {
-      return false;
     }
   }
   return true;
 };
 
-//COMAPRING PRIMITVE KEY VALUES
-const ab = { a: "1", b: "2" };
+const ab = { a: "1", b: "2"};
 const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab,ba), true); // => true
+assertEqual(eqObjects(ab, ba), true); // => true
 
-const abc = { a: "1", b: "2", c: "3" };
+const abc = { a: "1", b: "2", c: "3"};
 assertEqual(eqObjects(ab, abc), false); // => false
 
-//COMPARING ARRAY KEY VALUES
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 assertEqual(eqObjects(cd, dc), true); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false); // => false
-
