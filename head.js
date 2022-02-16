c400893f5351a07ebca2acc52d49a1e69546d2f2
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-16 15:18:48 +0000

    moved test to diff file

--- head.js
+++ head.js
@@ -1,10 +1,7 @@
-// commonJS
-const assertEqual = require('./assertEqual');
+
 
 // ES
 //import assertEqual from './assertEqual';
-
-
 
 //FUNCTION HEAD RETURNS THE FIRST ELEMENT OF A GIVEN ARRAY
 const head = function(array) {
@@ -14,12 +11,8 @@ const head = function(array) {
     return array[0];
 };
 
-//TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head[null], undefined);
-assertEqual(head([5]), 6);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Lighthouse");
-assertEqual(head[null], 0);
+module.exports = head;
+
+
 
 
