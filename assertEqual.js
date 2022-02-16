6f10059159ea950af110b84b1f73f80cc463c605
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-16 21:19:06 +0000

    fixed bug

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,6 @@
 //FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
+  if (actual.toString() === expected.toString()) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`⛔️ Assertion Failed: ${actual} !== ${expected}`);
