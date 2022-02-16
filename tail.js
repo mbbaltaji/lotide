5d98aad4dac4d9bdb2930c95e03a19efd9c993b2
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-16 21:35:23 +0000

    moved testing to diff file

--- tail.js
+++ tail.js
@@ -9,21 +9,4 @@ const tail = function(array) {
   return tailArray;
 };
 
-const assertEqual = function(actual, expected) {
-  // Arrays are converted to Strings Since we cannot compare two arrays using ===
-  if (actual.toString() === expected.toString()) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
- 
-assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
-assertEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
-assertEqual(tail(["One"]), []);
-assertEqual(tail([1,2,3]), [1,2,3]); //false
-
-//checking if orignal array is not modified
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertEqual(words.length, 3);
+module.exports = tail;
