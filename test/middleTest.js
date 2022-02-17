038269e814993b26cc5ec5e01e47d1ea191ebe11
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-17 02:18:34 +0000

    converted tests to mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,31 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-//1 OR 2 ELEMENTS
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-//ODD
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-//EVEN
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]);
+describe("#middle", () => {
+  
+  // 1 OR 2 ELEMENTS IN ARRAY
+  it('returns [] for [1]', () =>{
+    assert.deepEqual(middle([1]), []);
+  });
+  it('returns [] for [1, 2]', () =>{
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  // ODD NUM OF ELEMENTS IN ARRAY
+  it('returns [2] for [1, 2, 3] ', () =>{
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it('returns [3] for [1, 2, 3, 4, 5] ', () =>{
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  //EVEN NUM OF ELEMENTS IN ARRAY
+  it('returns [2, 3] for [1, 2, 3, 4] ', () =>{
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it('returns [3, 4] for [1, 2, 3, 4, 5, 6] ', () =>{
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+ 
+});
