d94c7229f50065c54d8bf7226f8fb55d0efb78ea
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-18 16:12:54 +0000

    added testing in mocha and chai

---
+++ test/mapTest.js
@@ -0,0 +1,37 @@
+const assert = require('chai').assert;
+const map = require('../map');
+
+describe('#map', () => { 
+  
+  const words = ["ground", "control", "to", "major", "tom"];
+  console.log('TEST ARRAY: ', words);
+  
+  it("should return [ 'g', 'c', 't', 'm', 't' ] for callBack word => word[0]", () => {
+    assert.deepEqual(map(words, word => word[0]), [ 'g', 'c', 't', 'm', 't' ]);
+  });
+
+  it("should return [6, 7, 2, 5, 3] for callBack word => word.length", () => {
+    assert.deepEqual(map(words, word => word.length), [6, 7, 2, 5, 3]);
+  });
+
+  it("should return ['GROUND', 'CONTROL', 'TO', 'MAJOR', 'TOM'] for callBack word => word.toUpperCase()", () => {
+    assert.deepEqual(map(words, word => word.toUpperCase()), ['GROUND', 'CONTROL', 'TO', 'MAJOR', 'TOM']);
+  });
+
+  
+  
+
+
+});
+
+
+
+
+
+// const result1 = map(words, word => word[0]);
+// const result2 = map(words, word => word.length);
+// const result3 = map(words, word => word.toUpperCase());
+
+// assertArraysEqual(result1,[ 'g', 'c', 't', 'm', 't' ]);
+// assertArraysEqual(result2, [6, 7, 2, 5, 3]);
+// assertArraysEqual(result3,['GROUND', 'CONTROL', 'TO', 'MAJOR', 'TOM']);
