57f791fb769c94486d98d2e8093d9ff99a7dd41a
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-18 15:56:28 +0000

    moved testing to test directory

--- eqObjects.js
+++ eqObjects.js
@@ -1,31 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`\x1b[32m✅ Assertion Passed: Objects are equal! ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔️ Assertion Failed: Objects are NOT equal ${actual} !== ${expected}`);
-  }
-};
-
-// CHECK IF TWO ARRAYS ARE EQUAL
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length)
-    return false;
-  
-  else if (arr1.every((element, index) => element === arr2[index])) {
-    return true;
-  }
-  return false;
-  //LONG WAY USING FOOR LOOP
-  // if (arr1.length !== arr2.length) {
-  //   return false;
-  // }
-  // for (let i = 0; i < arr1.length; i++) {
-  //   if (arr1[i] === arr2[i] && i === arr1.length - 1) {
-  //     return true;
-  //   }
-  // }
-  // return false;
-};
+const eqArrays = require('./eqArrays');
 
 const eqObjects = function(obj1, obj2) {
   // CHECK IF OBJECTS HAVE SAME NUMBER OF KEYS
@@ -47,16 +20,4 @@ const eqObjects = function(obj1, obj2) {
   return true;
 };
 
-const ab = { a: "1", b: "2"};
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
-
-const abc = { a: "1", b: "2", c: "3"};
-assertEqual(eqObjects(ab, abc), false); // => false
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
+module.exports = eqObjects;
