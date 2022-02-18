c264fabe328c49f7414bbcc8dd5e5793ea3a6419
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-18 15:29:11 +0000

    moved tests to test directory

--- findKey.js
+++ findKey.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // RETURNS FIRST KEY THAT MATCHES callBack
 const findKey = function(obj, callBack) {
   let foundKey = '';
@@ -18,11 +10,4 @@ const findKey = function(obj, callBack) {
   return foundKey;
 };
 
-assertEqual (findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), 'noma');
+module.exports = findKey;
