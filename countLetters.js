fa4ba12988546a0e78abc383105fda3251c21bea
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-18 14:07:49 +0000

    moved testing to test directory

--- countLetters.js
+++ countLetters.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const countLetters = function(sentence) {
   //REMOVE WHITE SPACE
   sentence = sentence.replace(/\s/g, '');
@@ -21,10 +13,4 @@ const countLetters = function(sentence) {
   return letterCount;
 };
 
-let actual = countLetters("lighthouse in the house");
-let expected = { l: 1, i: 2, g: 1, h: 4,
-  t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 };
-
-assertEqual(actual.toString(), expected.toString());
-assertEqual(actual.l, 1);
-assertEqual(actual.u, 2);
+module.exports = countLetters;
