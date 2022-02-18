5ba088e5f2fa0df2b7b5bba3b7f954910743342e
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-18 16:38:48 +0000

    moved testing to test directory

--- without.js
+++ without.js
@@ -1,11 +1,3 @@
-// CHECK IF TWO ARRAYS ARE EQUAL
-const eqArrays = function(arr1, arr2) {
-  if (arr1.every((element, index) => element === arr2[index])){
-    return true
-  }
-  return false;
-};
-
 // FILTERS itemsToRemove FROM ARRAY
 const without = function(array, itemsToRemove){
   let newArr = array;
@@ -18,31 +10,7 @@ const without = function(array, itemsToRemove){
   return result;
 }
 
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)){
-    console.log("✅ Assertion Passed: Arrays are equal! " + arr1 + ' === ' + arr2);
-  } else{
-    console.log("🛑 Assertion Failed: Arrays are not equal! " + arr1 + ' !== ' + arr2)
-  }
-}
-
-console.log(without([1, 2, 3], [1])); // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
-
-// CHECK IF ORIGINAL ARRAY IS ALTERED
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); 
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-
-// DRIVER CODE
-assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"] );
-assertArraysEqual(without(["hello", "world", "lighthouse"], 
-["lighthouse"]), ["hello", "world"] );
-assertArraysEqual(without([1, 2, 3], [1]), [2]);
-
-
+module.exports = without;
 
 
 
